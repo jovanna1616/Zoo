e0b296507847c4db81e70ba95da5e2e005b1b68f
Vivify Academy <ladybutterfly.fly@gmail.com>
2017-10-10 20:45:03 +0200

    button for new Animal

--- src/app/animal-list/animal-list.component.ts
+++ src/app/animal-list/animal-list.component.ts
@@ -9,27 +9,27 @@ import { Animal } from './animal';
 })
 export class AnimalListComponent implements OnInit {
 
-	animals:Array<Object> = 
-	[
-		new Animal('cat', 'Kitty', '02-09-2017'),
-		new Animal('dog', 'Bully', '01-09-2017'),
-		new Animal('snake', 'Python', ''),
-		new Animal('bear', 'Teddy', '02-10-2017'),
-		new Animal('fish', 'Rocky', ''),
+	animals: Array<Object> = [];
 
-	];
+	newAnimal: Object = new Animal('', '', '');
 	
-  removeAnimal(index) {
-	  return this.animals.splice(index, 1);
-  }
+  
+  	addAnimal() {
+		this.animals.push(this.newAnimal);
+		this.newAnimal = new Animal('', '', '');
+  	}
+
+	removeAnimal(index) {
+		return this.animals.splice(index, 1);
+	}
 
 
-  moveToTop(index, toIndex) {
-	  var toIndex:any = this.animals[0];
-	  var element = this.animals[index];
-	  this.animals.splice(index, 1);
-	  this.animals.splice(toIndex, 0, element);
-}
+	moveToTop(index, toIndex) {
+		var toIndex: any = this.animals[0];
+		var element = this.animals[index];
+		this.animals.splice(index, 1);
+		this.animals.splice(toIndex, 0, element);
+	}
 
   ngOnInit() {
 
