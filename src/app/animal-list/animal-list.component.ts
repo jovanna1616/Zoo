e59893ca7cdcb473897995ce3a4ada2a93235339
Vivify Academy <ladybutterfly.fly@gmail.com>
2017-10-10 19:01:01 +0200

    adding AnimalLIst

---
+++ src/app/animal-list/animal-list.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-animal-list',
+  templateUrl: './animal-list.component.html',
+  styleUrls: ['./animal-list.component.css']
+})
+export class AnimalListComponent implements OnInit {
+
+	animals:Array<Object> = 
+	[
+		{
+			species: 'cat',
+			name:'Kitty',
+			birth: '01-01-2016'
+
+		},
+		{
+			species: 'dog',
+			name: 'Bully',
+			birth: '01-01-2015'
+
+		},
+		{
+			species: 'snake',
+			name: 'Python',
+			birth: '01-01-2016'
+
+		},
+		{
+			species: 'bear',
+			name: 'Teddy',
+			birth: '01-01-2015'
+
+		},
+		{
+			species: 'monkey',
+			name: 'Tom',
+			birth: '01-01-2016'
+
+		},
+
+	];
+	
+  
+
+  ngOnInit() {
+  }
+
+}
